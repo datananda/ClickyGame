b6ac01fbeaebf49ccaa83f1cb4e2f4d0af8d6c1c
Madeleine Filloux <mfilloux@gmail.com>
2018-06-26 21:44:09 -0500

    keep track of score and rearrange kitties on click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import $ from "jquery";
 import ImageCard from "./components/ImageCard";
+import Navbar from "./components/Navbar";
 import API from "./utils/API";
 import "./App.css";
 
 class App extends Component {
   state = {
+    score: 0,
+    topScore: 0,
     kitties: []
   }
 
@@ -20,31 +23,56 @@ class App extends Component {
         const $urls = $data.find("url");
         const kitties = $urls.map(function() {
           return {
+            id: $(this).html().split("id=")[1].split("&amp;")[0],
             src: $(this).html(),
             wasClicked: false,
           }
-        });
+        }).get();
         this.setState({ kitties });
       })
       .catch(err => console.log(err));
   }
 
-  clickKitty = i => {
+  rearrangeKitties = () => {
     const kitties = this.state.kitties;
-    kitties[i].wasClicked = true;
+    for (let i = kitties.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = kitties[i];
+      kitties[i] = kitties[j];
+      kitties[j] = temp;
+    }
+    return kitties;
+  }
+
+  clickKitty = id => {
+    const kitties = this.state.kitties.map((kitty) => {
+      if (kitty.id === id) {
+        if (!kitty.wasClicked) {
+          this.setState({ score: this.state.score + 1 })
+        }
+        kitty.wasClicked = true;
+      }
+      return kitty;
+    });
     this.setState({ kitties });
+    this.setState({
+      kitties: this.rearrangeKitties()
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Navbar 
+          score={this.state.score}
+          topScore={this.state.topScore}
+        />
         <div className="container">
           <div className="row">
-            {this.state.kitties.map((i, kitty) => (
+            {this.state.kitties.map((kitty) => (
               <ImageCard 
-                key={i}
-                i={i}
+                key={kitty.id}
+                id={kitty.id}
                 src={kitty.src}
                 clickKitty={this.clickKitty}
               />
--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./ImageCard.css";
 
 const ImageCard = props => (
-    <div className="col-3" onClick={() => props.clickKitty(props.i)}>
+    <div className="col-3" onClick={() => props.clickKitty(props.id)}>
         <div className="kitty-container mb-4">
             <img className="img-thumbnail img-fluid kitty-image" src={props.src} alt="kitty" />
         </div>
